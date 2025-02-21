194243928ce9b744e2321c9c6348b15d1e5cd9d6
AndoniUTT <andoni2020lopez@gmail.com>
2025-02-21 13:16:28 -0600

    02 Modificacion de las cards

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -7,25 +7,28 @@ type DrinkCardProps = {
 
 export default function DrinkCard({ drink }: DrinkCardProps) {
     const selectRecipe = useAppStore(state => state.selectRecipe)
-  return (
-    <div className="border shadow-lg">
-        <div className="overflow-hidden">
-            <img 
-            src={drink.strDrinkThumb} 
-            alt={'Imagen de ' + drink.strDrink}
-            className="hover:scale-125 transition-transform hover:rotate-2"
-            />
+
+    return (
+        <div className="border border-green-500 shadow-xl rounded-2xl overflow-hidden bg-white">
+            <div className="overflow-hidden">
+                <img 
+                    src={drink.strDrinkThumb} 
+                    alt={'Imagen de ' + drink.strDrink}
+                    className="hover:scale-110 transition-transform hover:rotate-2 grayscale hover:grayscale-0 rounded-t-2xl"
+                />
+            </div>
+            <div className="p-5">
+                <h2 className="text-xl font-bold text-center text-blue-600 truncate">
+                    {drink.strDrink}
+                </h2>
+                <button
+                    type="button"
+                    onClick={() => selectRecipe(drink.idDrink)}
+                    className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 mt-5 w-full p-3 font-bold text-white text-lg rounded-lg shadow-md transition-transform hover:scale-105"
+                >
+                    Ver Receta
+                </button>
+            </div>
         </div>
-        <div className="p-5">
-            <h2 className="text-2xl truncate font-black">
-                {drink.strDrink}
-            </h2>
-            <button
-                type="button"
-                onClick={() => selectRecipe(drink.idDrink)}
-                className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
-                >Ver Receta</button>
-        </div>
-    </div>
-  )
+    )
 }
